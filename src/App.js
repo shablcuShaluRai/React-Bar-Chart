8cd6b49fe54f2518e0b693f3afa5a40bb3e6591e
shalurai <shaluraics22@gmail.com>
2017-11-04 18:05:52 +0900

    Show data using Bar,Pie,Line chart

--- src/App.js
+++ src/App.js
@@ -3,18 +3,48 @@ import * as GdpApi from './utils/GdpAPI'
 import Chart from  './Chart'
 
 
-
 class App extends Component {
 
+  state = {
+    chartData: {}
+      }
+  // componentDidMount = () => {
+  //   GdpApi.fetchData().then(data => console.log(data))
+  // }
 
-  componentDidMount = () => {
-    GdpApi.fetchData().then(data => console.log(data))
+  componentWillMount(){
+    this.getChartData()
   }
+
+getChartData(){
+  this.setState({
+    chartData : {
+      labels: ["NewDelhi", "Bangalore", "Lucknow", "Mumbai", "Rajsthan", "Shimla", "hydraBad"],
+      datasets: [{
+        label: "Populations",
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+          'rgba(255, 99, 132, 0.6)'
+        ],
+         borderWidth: 1,
+        data: [65034803,  80238239, 5983792,81298392, 56379382, 5529382, 4028392],
+      }]
+    }
+
+  })
+}
+
   render() {
     return (
       <div className="App">
         <h1>React Bar Chart</h1>
-        <Chart />
+        <Chart  chartData = { this.state.chartData}/>
+      
 
 
       </div>
--- src/Chart.js
+++ src/Chart.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react'
-import {Bar} from 'react-chartjs-2';
+import {Bar, Line, Pie} from 'react-chartjs-2';
 
-const data = {
-  labels: ["NewDelhi", "Bangalore", "Lucknow", "Mumbai", "Rajsthan", "Shimla", "hydraBad"],
-  datasets: [{
-    label: "Populations",
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.6)',
-      'rgba(54, 162, 235, 0.6)',
-      'rgba(255, 206, 86, 0.6)',
-      'rgba(75, 192, 192, 0.6)',
-      'rgba(153, 102, 255, 0.6)',
-      'rgba(255, 159, 64, 0.6)',
-      'rgba(255, 99, 132, 0.6)'
-    ],
-     borderWidth: 1,
-    data: [65034803,  80238239, 5983792,81298392, 56379382, 5529382, 4028392],
-  }]
-};
+
 
 class Chart extends Component {
 
-    state = {
-      chartData: data,
-        }
+
 
   render() {
-    const { chartData } = this.state;
+    const { chartData } = this.props;
     return (
       <div>
         <Bar
         data={chartData}
-        options={{}}
-        width="600"
-        height="250" />
+        options={{
+          title:{
+            display : true,
+            text: 'Largest Cities In Bangalore',
+            fontSize: 25
+          },
+          legend:{
+            display:true,
+            position:'right'
+          }
+        }}
+        width={600}
+        height={250} />
+        <Line
+        data={chartData}
+        options={{
+          title:{
+            display : true,
+            text: 'Largest Cities In Bangalore',
+            fontSize: 25
+          },
+          legend:{
+            display:true,
+            position:'right'
+          }
+        }}
+        width={600}
+        height={250} />
+
+        <Pie
+        data={chartData}
+        options={{
+          title:{
+            display : true,
+            text: 'Largest Cities In Bangalore',
+            fontSize: 25
+          },
+          legend:{
+            display:true,
+            position:'right'
+          }
+        }}
+        width={600}
+        height={250} />
       </div>
     )
   }
